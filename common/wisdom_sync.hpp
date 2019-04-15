3fd363668137abfb80eb48619c14a1e2177a09d5
Lorenzo Pistone <blaffablaffa@gmail.com>
2019-04-15 17:52:43 +0200

    better wisdom_sync
    allow syncing wisdom/reading wisdom from a file/not syncing with a single --wisdom_mode argument. Also force this wisdom to have bit-exact result across different runs.

--- common/wisdom_sync.hpp
+++ common/wisdom_sync.hpp
@@ -1,32 +1,50 @@
 #pragma once
 
 #include <string>
+#include <iostream>
 #include <boost/program_options.hpp>
 #include <fftw3.h>
+#include "utilities.hpp"
 #include "mpi_environment.hpp"
 
 struct wisdom_sync {
 
-	bool do_sync = true;
+	std::string wisdom_mode;
+	const int &fftw_flags = _fftw_flags;
 
 	void add_options(boost::program_options::options_description &options) {
 		options.add_options()
-				("no_wisdom_sync", boost::program_options::bool_switch()->notifier([=](bool set) { do_sync = !set; }),
-				 "do not sync fftw wisdom across MPI processes");
+				("wisdom_mode", boost::program_options::value(&wisdom_mode)->default_value("sync"),
+				 "Mode for wisdom syncing across MPI processes: \"sync\" calculates wisdom once and propagates to all processes, \"node\" does not propagate wisdom, other values are interpreted as filenames to read wisdom from");
 	}
 
 	void gather() const {
-		if (!do_sync || !mpi_global_coord) return;
-		std::string wisdom;
-		boost::mpi::broadcast(mpi_global, wisdom, 0);
 		fftw_forget_wisdom();
-		fftw_import_wisdom_from_string(wisdom.c_str());
+		if (wisdom_mode == "sync") {
+			if (!mpi_global_coord) return;
+			std::string wisdom;
+			boost::mpi::broadcast(mpi_global, wisdom, 0);
+			fftw_import_wisdom_from_string(wisdom.c_str());
+			_fftw_flags = FFTW_WISDOM_ONLY;
+		} else if (wisdom_mode != "none") {
+			fftw_import_wisdom_from_filename(wisdom_mode.c_str());
+			_fftw_flags = FFTW_WISDOM_ONLY;
+		}
 	}
 
 	void scatter() const {
-		if (!do_sync || mpi_global_coord) return;
 		std::string wisdom = fftw_export_wisdom_to_string();
-		boost::mpi::broadcast(mpi_global, wisdom, 0);
+		if (wisdom_mode == "sync" && !mpi_global_coord) boost::mpi::broadcast(mpi_global, wisdom, 0);
+		if (gconf.verbose) {
+			if (wisdom_mode == "none")
+				collect_ostream(std::cerr) << process_ident << ": FFTW wisdom:" << std::endl << wisdom << std::endl;
+			else if (!mpi_global_coord)
+				collect_ostream(std::cout) << ": FFTW wisdom:" << std::endl << wisdom << std::endl;
+		}
 	}
+
+private:
+
+	mutable int _fftw_flags = 0;
 
 };
