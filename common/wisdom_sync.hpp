5ae6c9574d5539eabb325fde9c528dcfc99fb1b7
Lorenzo Pistone <blaffablaffa@gmail.com>
2019-05-02 15:27:35 +0200

    allow specifying default mode for wisdom_sync

--- common/wisdom_sync.hpp
+++ common/wisdom_sync.hpp
@@ -11,9 +11,11 @@ struct wisdom_sync {
 
 	const int &fftw_flags = _fftw_flags;
 
+	wisdom_sync(const std::string& default_mode = "sync"): default_mode(default_mode) {}
+
 	void add_options(boost::program_options::options_description &options) {
 		options.add_options()
-				("wisdom_mode", boost::program_options::value(&wisdom_mode)->default_value("sync"),
+				("wisdom_mode", boost::program_options::value(&wisdom_mode)->default_value(default_mode),
 				 "Mode for wisdom syncing across MPI processes: \"sync\" calculates wisdom once and propagates to all processes, \"node\" does not propagate wisdom, other values are interpreted as filenames to read wisdom from");
 	}
 
@@ -45,6 +47,6 @@ struct wisdom_sync {
 private:
 
 	mutable int _fftw_flags = 0;
-	std::string wisdom_mode;
+	std::string wisdom_mode, default_mode;
 
 };
