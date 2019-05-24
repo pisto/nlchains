e6c200110963619769d56181ff9e1f6ee24d713e
Lorenzo Pistone <blaffablaffa@gmail.com>
2019-05-24 11:04:32 +0200

    calculate eigensystem only in one MPI process

--- dDNKG/eigensystem.cpp
+++ dDNKG/eigensystem.cpp
@@ -1,5 +1,7 @@
 #include <fstream>
 #include <stdexcept>
+#include <iostream>
+#include "../common/utilities.hpp"
 #include "../common/configuration.hpp"
 #include "../common/mpi_environment.hpp"
 #include "dDNKG.hpp"
@@ -9,15 +11,15 @@ using namespace std;
 namespace dDNKG {
 
 	void eigensystem(const arma::vec &mp2, arma::mat &eigenvectors, arma::vec &omegas) {
-		arma::mat interaction = diagmat(mp2);
-		interaction.diag(1).fill(-1);
-		interaction.diag(-1).fill(-1);
-		interaction(0, gconf.chain_length - 1) = interaction(gconf.chain_length - 1, 0) = -1;
-		if (!arma::eig_sym(omegas, eigenvectors, interaction))
-			throw runtime_error("Cannot calculate eigensystem!");
-		omegas = sqrt(omegas);
-
 		if (!mpi_global_coord) {
+			arma::mat interaction = diagmat(mp2);
+			interaction.diag(1).fill(-1);
+			interaction.diag(-1).fill(-1);
+			interaction(0, gconf.chain_length - 1) = interaction(gconf.chain_length - 1, 0) = -1;
+			if (!arma::eig_sym(omegas, eigenvectors, interaction))
+				throw runtime_error("Cannot calculate eigensystem!");
+			omegas = sqrt(omegas);
+
 			auto vecsize = gconf.chain_length * sizeof(double), matsize = size_t(gconf.chain_length) * gconf.chain_length * sizeof(double);
 			ofstream dump_eigensystem(gconf.dump_prefix + "-omegas");
 			dump_eigensystem.exceptions(ios::failbit | ios::badbit | ios::eofbit);
@@ -25,7 +27,12 @@ namespace dDNKG {
 			dump_eigensystem.close();
 			dump_eigensystem.open(gconf.dump_prefix + "-eigenvectors");
 			dump_eigensystem.write((char *) eigenvectors.memptr(), matsize);
+		} else {
+			eigenvectors = arma::mat(gconf.chain_length, gconf.chain_length);
+			omegas = arma::vec(gconf.chain_length);
 		}
+		boost::mpi::broadcast(mpi_global, eigenvectors.memptr(), eigenvectors.n_elem, 0);
+		boost::mpi::broadcast(mpi_global, omegas.memptr(), omegas.n_elem, 0);
 	}
 
 }
